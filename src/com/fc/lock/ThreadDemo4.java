d7895f9faab934eb3b6a53e706f7f8d3e14a6406
fengchen98 <249841036@qq.com>
2022-05-06 21:45:54 +0800

    JUC并发编程1

--- src/com/fc/lock/ThreadDemo4.java
+++ src/com/fc/lock/ThreadDemo4.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 public class ThreadDemo4 {
     public static void main(String[] args) {
         List<String> list= Collections.synchronizedList(new ArrayList<>());
+       // List<String> list= Collections.synchronizedList(new ArrayList<>());
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 list.add(UUID.randomUUID().toString().substring(0,8));
